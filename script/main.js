04c7cb5751e3623ca38055d091d6d06e2967ad00
Rayquen <rayquen.zero@gmail.com>
2021-11-22 02:05:15 -0300

    first fix

--- script/main.js
+++ script/main.js
@@ -6,5 +6,5 @@ $(document).ready(function(){
     $("#idtime").delay(600).animate({ opacity: 0.88 }, 700);
     $("#iddate").delay(600).animate({ opacity: 0.66 }, 900);
     $("#idforecast").delay(600).animate({ opacity: 0.66 }, 700);
-    $("#idfooter").delay(600).animate({ opacity: 1 }, 900);
+    $(".footer").delay(600).animate({ opacity: 0.66 }, 700);
 })
